87bdf6f8c82c4cfef467c7ea9b0f7e262baf9b4f
donnymboma01 <donnymboma01@gmail.com>
2023-09-28 14:13:14 +0200

    feat : signin complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import { errorHandler } from "../utils/errors.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,6 +17,35 @@ export const signup = async (req, res, next) => {
     console.log(req.body);
   } catch (error) {
     //res.status(500).json(error.message);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user_exists = await User.findOne({ email });
+    if (!user_exists) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      user_exists.password
+    );
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: user_exists._id }, process.env.SECRET_TOKEN, {
+      expiresIn: "1d",
+    });
+
+    const { password: hashedPassword, ...rest } = user_exists._doc;
+    const expireDate = new Date(Date.now() + 360000); // 1hr
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expireDate })
+      .status(200)
+      .json({ user: user_exists, token });
+  } catch (error) {
     next(error);
   }
 };
