5298302b417efc87196daf10f5a4407ad3b618df
donnymboma01 <donnymboma01@gmail.com>
2023-10-05 17:37:09 +0200

    feat: signin & signup with google done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,3 +49,42 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+// Auth with Google account.
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.SECRET_TOKEN);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 heure.
+      res
+        .cookie("access_token", token, { httpOnly: true, expiryDate })
+        .status(200)
+        .json(user);
+    } else {
+      const generatePassword = Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatePassword, 12);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.floor(Math.random() * 10000).toString(),
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photoURL,
+      });
+
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.SECRET_TOKEN);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+
+      const expiryDate = new Date(Date.now() + 3600000); // 1 heure.
+      res
+        .cookie("access_token", token, { httpOnly: true, expiryDate })
+        .status(200)
+        .json(newUser);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
